0b1de7a63d59a06f23e0ed3f55b7988a5e5beedf
Gregor Grigorjan <grgrig@ttu.ee>
2020-10-24 14:37:39 +0300

    fix mobile

--- fetcher.js
+++ fetcher.js
@@ -2,6 +2,7 @@ function fetchCat() {
     const catApi = "https://api.thecatapi.com/v1/images/search"
     
     document.getElementById("fetch-button").innerHTML = "Loading..."
+    document.getElementById("catpic").src = "images/200.gif"
 
     fetch(catApi)
         .then(res => res.json())
@@ -12,6 +13,7 @@ function fetchCat() {
         })
         .catch(err => {
             document.getElementById("fetch-button").innerHTML = "More cats!"
+            document.getElementById("catpic").src = "https://i.pinimg.com/originals/7a/af/0f/7aaf0f1d48f57b7779c0fbcf103c2d0f.jpg"
             alert("Sorry, you couldn't be connected to the cat database :(")
             throw err;
         });
